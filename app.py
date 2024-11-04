f53932f86ba78ac10654b9075c6520ded23762a1
David140614 <david.zr04@gmail.com>
2024-11-03 22:54:20 -0600

    Update app.py

--- app.py
+++ app.py
@@ -32,4 +32,5 @@ def upload_file():
     return jsonify({"message": "Archivo procesado", "result": result})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=10000)
+
